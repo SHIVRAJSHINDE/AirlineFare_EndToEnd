c1006e627b2c0621c82efdf3d92bf7502ce4e524
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2025-03-22 20:28:30 +0530

    CI One

--- src/Pipeline/s1_dataIngestion.py
+++ src/Pipeline/s1_dataIngestion.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 class DataIngestionClass:
-    def read_csv(source_path):
+    def read_csv(source_path:str):
         # source_path = r"D:/Data/01_AirlineData/Airline.csv"  # Raw string
 
         # Read the CSV file and return the DataFrame
@@ -36,16 +36,16 @@ class DataIngestionClass:
 # This block will only execute if this script is run directly
 if __name__ == "__main__":
     import os
-    source_path = os.path.join("D:", "\\Training", "04DataSets", "01_AirlineData", "Airline.csv")
+    # source_path = os.path.join("D:", "\\Training", "04DataSets", "01_AirlineData", "Airline.csv")
 
-    print(source_path)
-    # source_path = r"D:/Training/04DataSets/01_AirlineData/Airline.csv"
+    # print(source_path)
+    source_path = "D://Training/04DataSets/01_AirlineData/Airline.csv"
     # source_path = "D:\\DataSets\\01_AirlineData\\Airline.csv"  # Use Pathlib to build the path
     # df = pd.read_csv(r"C:/Users/SHIVRAJ SHINDE/JupiterWorking/XL_ML/Z_DataSets/01_AirlineData/Airline.csv")
     # print(df)
 
-    directory = "Data/01_RawData/"
+    directory = "./Data/01_RawData/"
     filename = "Airline.csv"
 
     df = DataIngestionClass.read_csv(source_path)  # Read the CSV file
-    # DataIngestionClass.save_file(df, directory, filename)  # Save the DataFrame to the destination
+    DataIngestionClass.save_file(df, directory, filename)  # Save the DataFrame to the destination
